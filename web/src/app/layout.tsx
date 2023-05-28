de84051736bd3208520a871494bd217f56cfbb87
rickydomingos <ricardodomingos97@gmail.com>
2023-05-28 11:01:37 -0300

    aula 2 visual web

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baijamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW SPACETIME',
+  description:
+    'One capsule time creates with React, NextJS, TypeScript and TailwindCSS!',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baijamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
